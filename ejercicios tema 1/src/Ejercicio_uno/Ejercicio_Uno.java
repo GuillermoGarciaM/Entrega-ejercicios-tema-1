7e26e0ce9ed48472947daa8fc60a7c027085c679
Familia <Familia@Usuario-PC>
2022-06-18 22:59:52 -0500

    Ejercicios tema 1

---
+++ ejercicios tema 1/src/Ejercicio_uno/Ejercicio_Uno.java
@@ -0,0 +1,16 @@
+package Ejercicio_uno;
+
+public class Ejercicio_Uno {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numero=10;
+		long numeroLong=4L;
+		double numeroDouble=2.0;
+		boolean Boolean=true;
+		String datoString="Esto es un String";
+		System.out.println("Los datos son: " + numero + ", " + numeroLong + ", " + numeroDouble + ", " + Boolean + ", " + 
+		datoString);
+	}
+
+}
